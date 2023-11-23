39cdcd1ca0abd22851d6e1feee898eab304671dd
Nikita Riabchii <151853023+NikitaRiabchii@users.noreply.github.com>
2023-11-23 16:42:48 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,15 @@
+# def num_sum(a):
+#     if type(a) == str:
+#         return "type error"
+#     if type(a) == bool:
+#         return "type error"
+#     if type(a) == int:
+#         e=0
+#         p = str(abs(a))
+#         for i in p:
+#            e+=int(i)
+#         return e
+#
+# print(num_sum(-146))
+# print(num_sum('-11'))
+# print(num_sum(True))
